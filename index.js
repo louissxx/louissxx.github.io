93398dce4dace643af37854c7d15f2bd589ef19c
louissxx <89163422+louissxx@users.noreply.github.com>
2023-04-02 20:45:05 -0400

    Update index.js
    testing connecting to infinityfree

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-fetch('http://localhost:3000/proxy')
+fetch('https://onephoto.infinityfreeapp.com/')
   .then(response => response.text())
   .then(data => console.log(data))
   .catch(error => console.error('Error:', error));
